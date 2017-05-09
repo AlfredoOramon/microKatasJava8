e599228d695eb306c64ba82cdb62bb23516a1f67
alfredo.gil <alfredo.gil@oramon.nl>
2017-05-09 12:14:22 +0200

    adding the test and the SUT(System Under Test)

---
+++ src/test/java/FilterCollectionTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * ClassName
+ * <p>
+ * Created by Alfredo Gil Rubio
+ * <p>
+ * Date: 9/05/17 12:11
+ */
+public class FilterCollectionTest
+{
+    FilterCollection SUT;
+}
