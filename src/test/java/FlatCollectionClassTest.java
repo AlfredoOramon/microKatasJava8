013608ba9f6ff209dfedaa428c66f08a34e81ca1
alfredo.gil <alfredo.gil@oramon.nl>
2017-05-10 09:14:36 +0200

    Flatmap test

---
+++ src/test/java/FlatCollectionClassTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.*;
+
+/**
+ * ClassName
+ * <p>
+ * Created by Alfredo Gil Rubio
+ * <p>
+ * Date: 10/05/17 9:10
+ */
+public class FlatCollectionClassTest
+{
+    //System Under Test
+    private FlatCollectionClass SUT;
+
+    @Before
+    public void setup()
+    {
+        FlatCollectionClass SUT=new FlatCollectionClass();
+    }
+
+}
